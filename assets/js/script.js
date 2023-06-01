30f907f9e3e9ec8402512041f4aaed2fa525d6ca
KTC96 <kyleclow96@gmail.com>
2023-06-01 13:55:51 +0000

    Add alerts for buttons and event listeners to check data type

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// Wait for the DOM to finish loading before running the game zz
+// Get the button elements and add event listeners to them \\
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    // returns all elements with the tag name button as an array, which we can then iterate through \\
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+
+        }) // Inside the code block 'this' refers to the button that was just clicked so if we click  the 
+          //addition button then 'this' is referring to that specific button.
+
+    }
+
+
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
