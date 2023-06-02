185b34e926a74662d8b93c5a5fd2650aa33e2fee
KTC96 <kyleclow96@gmail.com>
2023-06-02 11:20:58 +0000

    Added incrementation of correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // Wait for the DOM to finish loading before running the game zz
 // Get the button elements and add event listeners to them \\
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
     // returns all elements with the tag name button as an array, which we can then iterate through \\
 
     for (let button of buttons) {
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -39,37 +39,68 @@ function runGame(gameType) {
     }
 
 }
+
+
+/**
+ * Checks the answer against the first element in the returned calculateCorrectAnswer array
+ */
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+        incrementScore();
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
+    }
+
+    runGame(calculatedAnswer[1]);
+
+}
+
 /**
  * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns the correct answer.
  */
 
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
-    let operand1 = parseInt(document.getDocumentById('operand1').innerText); // When javascript gets data from the dom it is returned
+    let operand1 = parseInt(document.getElementById('operand1').innerText); // When javascript gets data from the dom it is returned
     // as a string and you cannot do mathematical operations on a string so that is why you use parseInt
-    let operand2 = parseInt(document.getDocumentById('operand2').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
 
-    if (operator === "+"){
+    if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else {
         alert(`Unimplemented operator ${operator} `);
-        throw `Unimplimented operator ${operator}. Aborting!`;
+        throw `Unimplemented operator ${operator}. Aborting!`;
     }
 
 }
-
-
-function checkAnswer() {
-
-}
-
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore; // ++ means to add one to the value, if you put the ++ after it will be written
+    // back to the DOM before having 1 added to it so won't update 
+
+
+    // inner text and text content are esentially interchangable with some small differences
+
 }
 
+/** Gets the tally of incorrect anwers from the DOM and increments it by 1 */
+
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
